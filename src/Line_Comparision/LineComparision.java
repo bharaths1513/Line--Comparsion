0612c10e2f705030f3af81925f6d544cfaa98e08
bharaths1513 <bharaths.1311@gmail.com>
2021-10-19 12:04:39 +0530

    Welcome to Line Comparision

---
+++ src/Line_Comparision/LineComparision.java
@@ -0,0 +1,11 @@
+package Line_Comparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparision Computation Program ");
+
+	}
+
+}
